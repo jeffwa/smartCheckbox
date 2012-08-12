b2e3c9a956d7b7a23463911758920c90874ca450
Jeffwa <jeffwa@gmail.com>
2012-08-12 13:30:39 +0200

    Updated README and smartCheckbox.jquery.json

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ smartCheckbox
 =============
 
 Smart Checkbox, a jQuery plugin that sets up parent and child checkboxes.
+
+### How it works
+
+The plugin takes a parent checkbox (or checkboxes), adds a click event to select/deselect its children and adds a change event to the
+children to set the appropriate state on the parent based on all the children.
+
+### Usage
+
+There are currently no options for smartCheckbox, so simply find the parent checkboxes and pass them to a smartCheckbox() call:
+
+`$(".smartCheckbox").smartCheckbox();`
+
+smartCheckbox() will automatically discard any matching elements that are not checkboxes.
+
+#### HTML
+
+##### Parent Checkbox
+
+The HTML for the parent checkbox is valid HTML:
+
+`<input type='checkbox' id='parent' name='parent' class='smartCheckbox' value='value' />`
+
+The ID of the parent checkbox is used as a class for it's children in order to tie them together.
+
+##### Child Checkbox
+
+For the children, also use simple, valid HTML:
+
+`<input type="checkbox" name="child" id="child1" class="parent1" value="child1">`
+
+A child may have multiple parents. Just assign the child mulitple classes matching parent checkboxes.
+
+`<input type="checkbox" name="child" id="child1" class="parent1 parent2 parent3" value="child1">`
---
+++ jquery.smartCheckbox.js
@@ -0,0 +1,64 @@
+/**
+ * Smart Checkbox v1.0
+ *
+ * Copyright (c) 2011 Jeff McLennan
+ * Dual licensed under the MIT or GPL Version 3 licenses.
+ */
+
+(function( $ ){
+    var _options = {};
+    
+    var methods = {
+        init: function( options ) {
+            
+            var that = this;
+            
+            return that.each(function() {
+                _options = $.extend( {}, $.fn.smartCheckbox.defaults, options );
+                
+                var $that = $(this);
+                if ( $that.attr("type") == "checkbox" ) {
+                    var masterId = $that.attr("id");
+                    if ( masterId ) {
+                        $that.click(function() {
+                            $(":checkbox."+masterId).each(function() {
+                                $(this).prop("checked", $that.prop("checked"));
+                            }).change();
+                        });
+                    
+                        $(":checkbox."+masterId).each(function(){
+                            var $el = $(this);
+                            $el.change(function(){
+                                var classes = $el.attr("class").split(" ");
+                                $.each( classes, function( idx, clazz ){
+                                    var _selectAll = $el.prop("checked");
+                                    if ( _selectAll ) {
+                                        $(":checkbox."+clazz).each(function() {
+                                            _selectAll = $(this).prop("checked");
+                                            return _selectAll;
+                                        });
+                                    }
+                                    $("#"+clazz).prop("checked", _selectAll);
+                                });
+                            });
+                        });
+                    }
+                }
+            });
+        }
+    }
+    
+    $.fn.smartCheckbox = function( method ) {
+        // Method calling logic
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            // Log any errors
+            Logger.error( 'Method ' +  method + ' does not exist!' );
+        }   
+    };
+    
+    $.fn.smartCheckbox.defaults = { }
+})( jQuery );
